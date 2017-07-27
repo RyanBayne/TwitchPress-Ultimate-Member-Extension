bb7c5dd1cba799396dab34a0b78f8f86da9ad6e0
Ryan R. Bayne <squeekycoder@gmail.com>
2017-07-27 23:53:43 +0100

    Initial Release

---
+++ readme.txt
@@ -0,0 +1,99 @@
+=== TwitchPress UM Extension ===
+Contributors: Ryan Bayne
+Donate link: https://www.patreon.com/zypherevolved
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: Twitch, TwitchPress, Twitch.tv, TwitchPress Extension, TwitchPress Boilerplate
+Requires at least: 4.4
+Tested up to: 4.8
+Stable tag: 1.0.0
+Minimum TwitchPress version: 1.2.3
+                        
+Integrate the Ultimate Member plugin with a TwitchPress system.
+                       
+== Description ==
+
+BETA WARNING - Plugin is still in development as part of a complex Twitch system when used with other extensions.
+The plugins development requires the core plugin to be updated and is time consuming in the early
+days. Please be patient and support the project to drive it forward. 
+
+This extension for your TwitchPress setup allows you to integrate the fantastic Ultimate Member plugin
+and control memberships in sync with Twitch.tv subscriptions. When your viewers change their subscription
+status, their membership level (role) will be updated. 
+
+Currently requires the following plugins:
+* TwitchPress - The core plugin, includes Twitch channel feed features.
+* TwitchPress Sync Extension - Focuses on updating locally stored Twitch data in a controlled manner.
+* TwitchPress Login Extension - This may be a temporary requirement during development.
+
+== Developer Comment ==
+
+With the TwitchPress project being so new the exact approach to take for each feature may not
+work first time around. The requirement of a core plugin and another two extensions might seem
+a lot but there is good reason for it. The TwitchPress project is being developed to allow the
+growth of an advanced Twitch suite. That can only happen in a highly extendable environment that 
+allows you to replace an official extension with a third-party one. An extendable environment
+requires individual systems and large features to be separated from the core plugin. 
+
+The key during this early stage is to let me know what you need your Twitch suit to do for you
+or for your visitors. I will do the work required to achieve the administration views and 
+public features to meet your needs.
+
+= Links =                                                                
+*   <a href="https://twitchpress.wordpress.com" title="">Blog</a>
+*   <a href="https://github.com/RyanBayne/TwitchPress" title="">GitHub</a>       
+*   <a href="https://twitter.com/ryan_r_bayne" title="Follow the projects Tweets.">Developers Twitter</a>     
+*   <a href="https://twitter.com/twitchpress" title="Follow the projects Tweets.">Plugins Twitter</a>     
+*   <a href="https://www.twitch.tv/zypherevolved" title="Follow my Twitch channel.">Authors Twitch</a>     
+*   <a href="https://discord.gg/NaRB3wE" title="Chat about TwitchPress on Discord.">Discord Chat</a>          
+*   <a href="https://www.patreon.com/zypherevolved" title="">Patreon Donations</a>     
+*   <a href="https://www.paypal.me/zypherevolved" title="">PayPal Donations</a>       
+
+= Features List = 
+
+* Coming Soon
+
+== Installation ==
+
+1. Method 1: Move folder inside the .zip file into the "wp-content/plugins/" directory if your website is stored locally. Then upload the new plugin folder using your FTP program.
+1. Method 2: Use your hosting control panels file manager to upload the plugin folder (not the .zip, only the folder inside it) to the "wp-content/plugins/" directory.
+1. Method 3: In your WordPress admin click on Plugins then click on Add New. You can search for your plugin there and perform the installation easily. This method does not apply to premium plugins.
+
+== Frequently Asked Questions ==
+
+= Can I hire you to customize the plugin for me? =
+Yes you can pay the plugin author to improve the plugin to suit your needs. Many improvements will be done free so
+post your requirements on the plugins forum first. 
+
+== Screenshots ==
+
+
+== Languages ==
+
+Translator needed to localize our Channel Solution for Twitch: TwitchPress, and it's extensions.
+
+== Upgrade Notice ==
+
+No special upgrade instructions this time. Just remember to 
+backup your site files and database.
+
+== Changelog ==
+
+= 1.0.0 =
+* DEV - Released ???
+
+== Version Numbers and Updating ==
+
+Explanation of versioning used by myself Ryan Bayne. The versioning scheme I use is called "Semantic Versioning 2.0.0" and more
+information about it can be found at http://semver.org/ 
+
+These are the rules followed to increase the TwitchPress plugin version number. Given a version number MAJOR.MINOR.PATCH, increment the:
+
+MAJOR version when you make incompatible API changes,
+MINOR version when you add functionality in a backwards-compatible manner, and
+PATCH version when you make backwards-compatible bug fixes.
+Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
+
+
+                  
---
+++ twitchpress-um-extension.php
@@ -0,0 +1,357 @@
+<?php 
+/*
+Plugin Name: TwitchPress UM Extension
+Version: 1.0.0
+Plugin URI: http://twitchpress.wordpress.com
+Description: Integrate the Ultimate Member and TwitchPress plugins.
+Author: Ryan Bayne
+Author URI: http://ryanbayne.wordpress.com
+Text Domain: twitchpress-um
+Domain Path: /languages
+Copyright: © 2017 Ryan Bayne
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ 
+GPL v3 
+
+This program is free software downloaded from WordPress.org: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. This means
+it can be provided for the sole purpose of being developed further
+and we do not promise it is ready for any one persons specific needs.
+See the GNU General Public License for more details.
+
+See <http://www.gnu.org/licenses/>.
+
+
+    Planning to create a TwitchPress extension like this one?
+
+    Step 1: Read WordPress.org plugin development guidelines
+    https://developer.wordpress.org/plugins/wordpress-org/detailed-plugin-guidelines/
+
+    Step 2: Read the TwitchPress extension development guidelines.
+    Full guide coming soon!
+    
+    
+*/
+
+// Prohibit direct script loading
+defined( 'ABSPATH' ) || die( 'Direct script access is not allowed!' );
+
+/**
+ * Check if TwitchPress is active, else avoid activation.
+ **/
+if ( !in_array( 'channel-solution-for-twitch/twitchpress.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    return;
+}
+
+/**
+* Check if Ultimate MEmber is active, else avoid activation.
+*/
+if ( !in_array( 'ultimate-member/index.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    return;
+}
+
+/**
+ * Required minimums and constants
+ */
+define( 'TWITCHPRESS_UM_VERSION', '1.0.0' );
+define( 'TWITCHPRESS_UM_MIN_PHP_VER', '5.6.0' );
+define( 'TWITCHPRESS_UM_MIN_TP_VER', '1.2.6' );
+define( 'TWITCHPRESS_UM_MAIN_FILE', __FILE__ );
+define( 'TWITCHPRESS_UM_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
+define( 'TWITCHPRESS_UM_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+if ( ! class_exists( 'TwitchPress_UM' ) ) :
+
+    class TwitchPress_UM {
+        /**
+         * @var Singleton
+         */
+        private static $instance;        
+
+        /**
+         * Get a *Singleton* instance of this class.
+         *
+         * @return Singleton The *Singleton* instance.
+         * 
+         * @version 1.0
+         */
+        public static function get_instance() {
+            if ( null === self::$instance ) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        } 
+        
+        /**
+         * Private clone method to prevent cloning of the instance of the
+         * *Singleton* instance.
+         *
+         * @return void
+         */
+        private function __clone() {}
+
+        /**
+         * Private unserialize method to prevent unserializing of the *Singleton*
+         * instance.
+         *
+         * @return void
+         */
+        private function __wakeup() {}    
+        
+        /**
+         * Protected constructor to prevent creating a new instance of the
+         * *Singleton* via the `new` operator from outside of this class.
+         */
+        protected function __construct() {
+            
+            $this->define_constants();
+            $this->includes();
+            $this->init();            
+
+        }
+
+        /**
+         * Define TwitchPress Login Constants.
+         * 
+         * @version 1.0
+         */
+        private function define_constants() {
+            
+            $upload_dir = wp_upload_dir();
+            
+            // Main (package) constants.
+            if ( ! defined( 'TWITCHPRESS_UM_ABSPATH' ) )  { define( 'TWITCHPRESS_UM_ABSPATH', __FILE__ ); }
+            if ( ! defined( 'TWITCHPRESS_UM_BASENAME' ) ) { define( 'TWITCHPRESS_UM_BASENAME', plugin_basename( __FILE__ ) ); }
+            if ( ! defined( 'TWITCHPRESS_UM_DIR_PATH' ) ) { define( 'TWITCHPRESS_UM_DIR_PATH', plugin_dir_path( __FILE__ ) ); }
+            
+            // Constants for force hidden views to been seen for this plugin.
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_USERS' ) )    { define( 'TWITCHPRESS_SHOW_SETTINGS_USERS', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_BOT' ) )      { define( 'TWITCHPRESS_SHOW_SETTINGS_BOT', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_CHAT' ) )     { define( 'TWITCHPRESS_SHOW_SETTINGS_CHAT', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_JUKEBOX' ) )  { define( 'TWITCHPRESS_SHOW_SETTINGS_JUKEBOX', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_GAMES' ) )    { define( 'TWITCHPRESS_SHOW_SETTINGS_GAMES', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_COMMANDS' ) ) { define( 'TWITCHPRESS_SHOW_SETTINGS_COMMANDS', true ); }
+            if ( ! defined( 'TWITCHPRESS_SHOW_SETTINGS_CONTENT' ) )  { define( 'TWITCHPRESS_SHOW_SETTINGS_CONTENT', true ); }      
+        }  
+
+        /**
+         * Include required files.
+         * 
+         * @version 1.0
+         */
+        public function includes() {
+            //include_once( 'includes/function.twitchpress-sync-core.php' );
+            
+            if ( twitchpress_is_request( 'admin' ) ) {
+                //include_once( 'includes/class.twitchpress-sync-uninstall.php' );
+            }      
+        }
+
+        /**
+         * Hook into actions and filters.
+         * 
+         * @version 1.0
+         */
+        private function init() {
+        
+            // Load this extension after plugins loaded, we need TwitchPress core to load first mainly.
+            add_action( 'plugins_loaded',      array( $this, 'after_plugins_loaded' ), 0 );
+
+            // do_action in TwitchPress Sync Extension when visitor subscribes through WP API.
+            add_action( 'twitchpress_sync_new_subscriber', array( $this, 'process_new_subscriber' ), 1 );
+            
+            add_action( 'twitchpress_sync_discontinued_subscriber', array( $this, 'discontinued_subscriber' ), 1 );
+                        
+            register_activation_hook( __FILE__, array( 'TwitchPress_Boilerplate_Install', 'install' ) );
+            
+            // Do not confuse deactivation of a plugin with deletion of a plugin - two very different requests.
+            register_deactivation_hook( __FILE__, array( 'TwitchPress_Boilerplate_Uninstall', 'deactivate' ) );
+        }
+                      
+        /**
+         * Init the plugin after plugins_loaded so environment variables are set.
+         * 
+         * @version 1.0
+         */
+        public function after_plugins_loaded() {
+            
+            // Filters
+            add_filter( 'twitchpress_get_sections_users', array( $this, 'settings_add_section_users' ), 8 );
+            add_filter( 'twitchpress_get_settings_users', array( $this, 'settings_add_options_users' ), 8 );
+            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );                  
+                    
+        }
+        
+        /**
+        * An action hooked using twitchpress_new_subscriber.
+        * 
+        * @version 1.0
+        */
+        public function process_new_subscriber( $user_id, $channel_name, $twitch_api_response ) {
+            
+            wp_die( 'Use API call result to get subscription package, then
+            get the UM role linked to that package, apply the UM role
+            to the user.');
+            
+            global $ultimatemember;
+
+            $res = $ultimatemember->query->get_roles( true );
+
+            var_dump( $res );    
+        }
+
+        /**
+        * An action hooked using twitchpress_discontinued_subscriber.
+        * 
+        * @version 1.0
+        */        
+        public function discontinued_subscriber( $user_id, $channel_name, $twitch_api_response ) {
+            wp_die( 'Get the UM role setup for those who do not subscribe to
+            Twitch.');            
+        }
+        
+        /**
+        * Add a new section to the User settings tab.
+        * 
+        * @param mixed $sections
+        * 
+        * @version 1.0
+        */
+        public function settings_add_section_users( $sections ) {  
+            global $only_section;
+            
+            // We use this to apply this extensions settings as the default view...
+            // i.e. when the tab is clicked and there is no "section" in URL. 
+            if( empty( $sections ) ){ 
+                $only_section = true;
+            } else { 
+                $only_section = false; 
+            }
+                        
+            // Add sections to the User Settings tab. 
+            $new_sections = array(
+                'ultimatemember'  => __( 'Ultimate Member Plugin', 'twitchpress-um' ),
+            );
+
+            return array_merge( $sections, $new_sections );           
+        }
+        
+        /**
+        * Add options to this extensions own settings section.
+        * 
+        * @param mixed $settings
+        * 
+        * @version 1.0
+        */
+        public function settings_add_options_users( $settings ) {
+            global $current_section, $only_section;
+            
+            $new_settings = array();
+            
+            // This first section is default if there are no other sections at all.
+            if ( 'ultimatemember' == $current_section || !$current_section && $only_section ) {
+                
+                // Get Ultimate Member roles. 
+                $um_roles = um_get_roles();
+                            
+                $new_settings = apply_filters( 'twitchpress_ultimatemember_users_settings', array(
+     
+                    array(
+                        'title' => __( 'Subscription to Role Pairing', 'twitchpress-um' ),
+                        'type'     => 'title',
+                        'desc'     => __( 'Pair your Twitch subscription plans to Ultimate Member roles.', 'twitchpress-um' ),
+                        'id'     => 'subscriptionrolepairing',
+                    ),
+
+                    array(
+                        'title'    => __( 'Prime', 'twitchpress-um' ),
+                        'desc'     => __( 'test2.', 'twitchpress-um' ),
+                        'id'       => 'twitchpress_um_subscriptiontorole_menu_prime',
+                        'css'      => 'min-width:300px;',
+                        'default'  => 'menu_order',
+                        'type'     => 'select',
+                        'options'  => apply_filters( 'twitchpress_um_subscriptiontorole_menu_prime', $um_roles ),
+                    ),                    
+                    
+                    array(
+                        'title'    => __( '$4.99', 'twitchpress-um' ),
+                        'desc'     => __( 'test2.', 'twitchpress-um' ),
+                        'id'       => 'twitchpress_um_subscriptiontorole_menu_1000',
+                        'css'      => 'min-width:300px;',
+                        'default'  => 'menu_order',
+                        'type'     => 'select',
+                        'options'  => apply_filters( 'twitchpress_um_subscriptiontorole_menu_1000', $um_roles ),
+                    ),
+                      
+                    array(
+                        'title'    => __( '$9.99', 'twitchpress-um' ),
+                        'desc'     => __( 'test2.', 'twitchpress-um' ),
+                        'id'       => 'twitchpress_um_subscriptiontorole_menu_2000',
+                        'css'      => 'min-width:300px;',
+                        'default'  => 'menu_order',
+                        'type'     => 'select',
+                        'options'  => apply_filters( 'twitchpress_um_subscriptiontorole_menu_2000', $um_roles ),
+                    ),
+                      
+                    array(
+                        'title'    => __( '$24.99', 'twitchpress-um' ),
+                        'desc'     => __( 'test2.', 'twitchpress-um' ),
+                        'id'       => 'twitchpress_um_subscriptiontorole_menu_3000',
+                        'css'      => 'min-width:300px;',
+                        'default'  => 'menu_order',
+                        'type'     => 'select',
+                        'options'  => apply_filters( 'twitchpress_um_subscriptiontorole_menu_3000', $um_roles ),
+                    ),
+                            
+                    array(
+                        'type'     => 'sectionend',
+                        'id'     => 'membershiprolepairing'
+                    ),
+
+                ));   
+                
+            }
+            
+            return array_merge( $settings, $new_settings );         
+        }
+        
+        /**
+         * Adds plugin action links
+         *
+         * @since 1.0.0
+         */
+        public function plugin_action_links( $links ) {
+            $plugin_links = array(
+
+            );
+            return array_merge( $plugin_links, $links );
+        }        
+
+        /**
+         * Get the plugin url.
+         * @return string
+         */
+        public function plugin_url() {                
+            return untrailingslashit( plugins_url( '/', __FILE__ ) );
+        }
+
+        /**
+         * Get the plugin path.
+         * @return string
+         */
+        public function plugin_path() {              
+            return untrailingslashit( plugin_dir_path( __FILE__ ) );
+        }                                                         
+    }
+    
+    $GLOBALS['twitchpress_um'] = TwitchPress_UM::get_instance();
+
+endif;    
+
